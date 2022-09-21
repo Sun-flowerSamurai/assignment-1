a293669d5cf4d47e4182cb720fff15b7c5f437ec
Verhoeven <m.p.j.verhoeven@student.tue.nl>
2022-09-21 15:27:55 +0200

    updated_foldE_help

--- src/FormulaManipulator.hs
+++ src/FormulaManipulator.hs
@@ -16,9 +16,18 @@ module FormulaManipulator
   )
 where
 
-import           ExprLanguage                   ( Expr(Var, Const, Plus, Mult) )
+import           ExprLanguage                   ( Expr(Var, Const, Plus, Mult), parseExpr )
 
-foldE     = error "Implement, document, and test this function"
+-- foldE :: Expr a b -> Expr a b -> (Expr a b -> Expr a b -> Expr a b) -> (Expr a b -> Expr a b -> Expr a b) -> Expr a b -> Expr a b -- het idee is om foldE op elke constructor te laten werken! dus foldE.a.b.f.g werkt op een Expr type. Hier hoort a bij const, b bij var, f bij plus en g bij mult (bijvoorbeeld)
+-- foldE     = error "Implement, document, and test this function"
+-- Willen we tuples? Ik weet niet helemaal of dit handig is
+foldE :: (a,b) -> (a,b) -> ((a, b) -> (a,b) -> (a,b)) -> ((a,b) -> (a,b) -> (a,b)) -> Expr c d -> (a,b)
+foldE a b f g = rec
+                where
+                  rec (Var v) = a --Constructor 1
+                  rec (Const c) = b --Constructor 2
+                  rec (Plus exr1 exr2) = f (rec exr1) (rec exr2) --Constructor 3 
+                  rec (Mult exr1 exr2) = g (rec exr1) (rec exr2) --Constructor 4
 printE    = error "Implement, document, and test this function"
 evalE     = error "Implement, document, and test this function"
 simplifyE = error "Implement, document, and test this function"
