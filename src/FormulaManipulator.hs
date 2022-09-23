bb3d2c9050d7b5640fd62ff50c22f8c0b09c8b26
David <d.v.r.chen@student.tue.nl>
2022-09-23 21:10:51 +0200

    Added nicer documentation to the functions, maybe a bit too much / not descriptive enough ?

--- src/FormulaManipulator.hs
+++ src/FormulaManipulator.hs
@@ -20,29 +20,32 @@ import           ExprLanguage                   ( Expr(Var, Const, Plus, Mult), 
 import Data.Bifoldable (Bifoldable)
 
 
-foldE :: (a -> c)
-  -> (b -> c)
-  -> (c -> c -> c)
-  -> (c -> c -> c)
-  -> Expr a b
-  -> c
--- ^Catamorphism factory for the Expr type
+foldE :: (a -> c)  -- Var 
+  -> (b -> c)      -- Const
+  -> (c -> c -> c) -- Plus 
+  -> (c -> c -> c) -- Mult
+  -> Expr a b      -- Expression to be folded
+  -> c             -- Result
+-- ^Catamorphism factory for the Expr type.
+-- the Expr type has a (binary) tree structure
 foldE f g h k = rec
                     where
-                    rec (Var v) = f v
-                    rec (Const c) = g c
-                    rec (Plus exr1 exr2) = h (rec exr1) (rec exr2)
-                    rec (Mult exr1 exr2) = k (rec exr1) (rec exr2)
+                    rec (Var v) = f v                              -- Var
+                    rec (Const c) = g c                            -- Const
+                    rec (Plus exr1 exr2) = h (rec exr1) (rec exr2) -- Plus
+                    rec (Mult exr1 exr2) = k (rec exr1) (rec exr2) -- Mult
 
 
-printE :: Show b => Expr String b -> String
+printE :: Show b => Expr String b -- Expression to be pretty-printed
+  -> String                       -- Pretty-printed expression
 -- ^Pretty-prints an expression, preserving the right order of operations
---  by placing parentheses appropriately 
+--  by placing parentheses (very generously)
 printE = foldE id show (\ l r -> "(" ++ l ++ "+" ++ r ++ ")") (\ l r -> l ++ "*" ++ r)
   
   
-  
-printE' :: Show b => Expr String b -> String
+printE' :: Show b => Expr String b -- Expression to be pretty-printed
+  -> String                        -- Pretty-printed expression
+-- ^Another take on pretty-printing, the solution is unfortunately very inefficient :(
 printE' = foldE id show (\ l r -> l ++ "+" ++ r) printMult
   where 
     printMult l r
@@ -51,12 +54,17 @@ printE' = foldE id show (\ l r -> l ++ "+" ++ r) printMult
       | '+' `elem` r                 = l ++ "*(" ++ r ++")"
       | otherwise                    = l ++ "*" ++ r
 
-evalE :: (a -> Integer) -> Expr a Integer -> Integer
--- ^Evaluates an expression where (some) variables are replaced by other expressions
+
+evalE :: (a -> Integer) -- dictionary containing the values
+  -> Expr a Integer     -- expression to be evaluated
+  -> Integer            -- final result
+-- ^Evaluates an expression where (some) variables are replaced 
+-- by other expressions (which in this case have to be constants)
 evalE d = foldE d id (+) (*)
 
 
-simplifyE :: (Num b, Eq b) => Expr a b -> Expr a b
+simplifyE :: (Num b, Eq b) => Expr a b -- Expression to be simplified
+  -> Expr a b                          -- Simplified expression
 -- ^Simplifies expressions using the following rules:
 --    For addition:
 --      constants are added together
@@ -81,7 +89,6 @@ simplifyE = foldE Var Const addE multE
       | c == 1                  = e
       | otherwise               = Mult (Const c) e
     multE e1 e2                 = Mult e1 e2
-
 
 
 -- Still need to implement diffE :(
