6c4ea931131b2f58db7664879df7fcfb84e5add4
David <d.v.r.chen@student.tue.nl>
2022-09-29 19:51:59 +0200

    Added documentation

--- src/FormulaManipulator.hs
+++ src/FormulaManipulator.hs
@@ -60,11 +60,12 @@ simplifyE :: (Num b, Eq b) => Expr a b -- Expression to be simplified
   -> Expr a b                          -- Simplified expression
 -- ^Simplifies expressions using the following rules:
 --    For addition:
---      constants are added together
---      adding zero to an expression is the same as that expression
+--      Constants are added together
+--      Adding zero to an expression is the same as that expression
 --    For multiplication:
---      Multiplying an expression by 0 return Const 0
---      Multiplying an expression by 1 simply return the expression
+--      Constants are multiplied together
+--      Multiplying an expression by 0 returns Const 0
+--      Multiplying an expression by 1 simply returns the expression
 simplifyE = foldE Var Const addE multE 
   where 
     addE (Const c1) (Const c2)  = Const (c1 + c2) 
@@ -84,30 +85,17 @@ simplifyE = foldE Var Const addE multE
     multE e1 e2                 = Mult e1 e2
 
 
--- Still need to implement diffE :(
-
--- tdiffE :: a -> (Expr a b, Expr a b ) -> (Expr a b, Expr a b) -- (f', f)
-
---tdiffE :: String -> (Expr String Int, Expr String Int) -> (Expr String Int, Expr String Int)
---tdiffE x (expr1, expr2) = foldE (tf (diffvar x, Var)) (tf (diffconst, Const)) (tf2 (diffplus, Plus)) (tf2 (diffmult, Mult)) (expr1, expr2)
---  where
---    tf (f,g) (a, b) = (f a, g b) -- Deze twee functies zorgen ervoor dat we goed tuples kunnen verwerken
---    tf2 (f, g) (a, b) (c, d) = (f a c, g b d)
---    diffvar x v = if v == x then (Const 1) else (Const 0) -- Zou miss beter kunnen
---    diffconst expr = 0 
---    diffplus expr1 expr2 = (Plus expr1 expr2) --doet nu niks--
---    diffmult expr1 expr2 = (Mult expr1 expr2) --same
-
-    -- werken allemaal niet 
-    -- diffplus expr1 expr2 = Plus (fst . tdiffE x expr1) (fst . tdiffE x expr2) 
-    -- diffmult expr1 expr2 = Plus (Mult (expr1) (fst . tdiffE x expr2)) (Mult (expr2) (fst . tdiffE x expr1)) 
-
-myDiffE :: (Eq a, Num b) => a -> Expr a b -> Expr a b
-myDiffE x = snd . foldE myVar myConst myAdd myMult
+diffE :: (Eq a, Num b) => a -- The variable with respect to which to differentiate.
+  -> Expr a b               -- The expression that needs to be differentiated
+  -> Expr a b               -- The resulting derivative
+-- ^Differentiates an expression with respect to some variable.
+-- The differentiation rules that are implemented are:
+--     The constant rule: differentiating a constant results in 0
+--     The sum rule: (f(x) + g(x))' = f'(x) + g'(x)
+--     The product rule: (f(x)*g(x))' = f'(x)*g(x) + f(x)*g'(x)
+diffE x = snd . foldE myVar myConst myAdd myMult
       where
         myVar v                    = (Var v, if v == x then Const 1 else Const 0)
         myConst c                  = (Const c, Const 0)
         myAdd (le, le') (re, re')  = (Plus le re, Plus le' re')
         myMult (le, le') (re, re') = (Mult le re, Plus (Mult le' re) (Mult le re'))
-
-diffE    = error "Implement, document, and test this function"
